e1025fdbf38043cb63a5705ff29577dd14339e18
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2010-11-16 12:25:02 +0000

    initial revision

---
+++ aloe.php
@@ -0,0 +1,24 @@
+<?php
+
+echo $_REQUEST['msg'];
+
+
+$to = $_REQUEST['to'];
+$from = $_REQUEST['from'];
+$message = $_REQUEST['msg'];
+
+if (empty($to)) {
+	$to = 'nuno.morgadinho@gmail.com';
+}
+if (empty($from)) {
+	$from = 'nuno.morgadinho@gmail.com';
+}
+if (empty($message)) {
+	$message = 'I was just testing the send button..';
+}
+mail($to, 'You have a new message from..'.$from, $message);
+
+?>
+<script type="text/javascript">
+history.go(-1);
+</script>
